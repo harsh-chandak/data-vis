fa512ba1ea9803cc0e05a7a1d314e92c0c81e2b6
harsh-chandak <harshnchandak@gmail.com>
2024-11-12 22:29:27 -0700

    Base Setup Done

---
+++ heatmap.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const svg = d3.select("#heatmap-svg")
+})
---
+++ stacked_bar_chart.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const svg = d3.select("#stacked-bar-chart-svg")
+})
